f840ec469faa3dbc649c0c4aed2a0afa40622b12
IlyasOussama <zghariilyasoussama@gmail.com>
2023-05-21 14:15:04 +0100

    initial setup

---
+++ src/components/apolloWrapper.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { ApolloClient, ApolloLink, HttpLink } from "@apollo/client";
+import {
+  ApolloNextAppProvider,
+  NextSSRInMemoryCache,
+  SSRMultipartLink,
+} from "@apollo/experimental-nextjs-app-support/ssr";
+
+function makeClient() {
+  const httpLink = new HttpLink({
+    uri: "https://api.lens.dev/",
+  });
+
+  return new ApolloClient({
+    cache: new NextSSRInMemoryCache(),
+    link:
+      typeof window === "undefined"
+        ? ApolloLink.from([
+            new SSRMultipartLink({
+              stripDefer: true,
+            }),
+            httpLink,
+          ])
+        : httpLink,
+  });
+}
+
+const ApolloWrapper = ({ children }: React.PropsWithChildren) => {
+  return (
+    <ApolloNextAppProvider makeClient={makeClient}>
+      {children}
+    </ApolloNextAppProvider>
+  );
+};
+
+export default ApolloWrapper;
